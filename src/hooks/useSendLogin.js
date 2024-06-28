a6e188af15faf11ec6e458dbe23347e295c462f5
natasha <natasha87050@gmail.com>
2024-06-28 16:59:52 -0300

    Cambios de anchor a Link

--- src/hooks/useSendLogin.js
+++ src/hooks/useSendLogin.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const useSendLogin = () => {
+    const navigate = useNavigate();
     const [credenciales, setCredenciales] = useState({
         usuario: "",
         contraseña: "",
@@ -47,7 +48,8 @@ const useSendLogin = () => {
             console.log("Sesión iniciada con éxito:", responseData);
 
             // Redirigir al usuario a la página de productos
-            window.location.reload()
+            navigate("/");
+            window.location.reload();
         } catch (error) {
             console.error("Error al enviar la solicitud:", error.message);
         }
